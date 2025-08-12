94de48cb430a63f9840f0029d31a95764f5dd174
Rebeca Pedrozo Cueto <rebecapedrozocueto@gmail.com>
2025-08-12 17:04:26 +0000

    Borrado duplicados

--- borradoDuplicados.py
+++ borradoDuplicados.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+
 emisiones = pd.read_csv('/content/Ventas3Agosto.csv')
-emisiones.dtypes
 emisiones['prima'] = pd.to_numeric(emisiones['prima'], errors='coerce')
+
 agrupado = emisiones.groupby('numero_poliza').agg({
     'clave_agente': 'first',
     'codigo_producto': 'first',
@@ -17,19 +18,23 @@ agrupado = emisiones.groupby('numero_poliza').agg({
     'CORREO_1': lambda x: list(pd.unique(x)),
     'CELULAR_1': lambda x: list(pd.unique(x)),
 }).reset_index()
-def expandir_columnas(emisiones, columna_base, prefijo):
-    max_items = emisiones[columna_base].apply(len).max()
-    nuevas_col = pd.DataFrame(emisiones[columna_base].tolist(), columns=[f"{prefijo}_{i+1}" for i in range(max_items)])
-    return pd.concat([emisiones.drop(columns=[columna_base]), nuevas_col], axis=1)
+
+def expandir_columnas(df, columna_base, prefijo):
+    max_items = df[columna_base].apply(len).max()
+    nuevas_cols = pd.DataFrame(
+        df[columna_base].tolist(),
+        columns=[f"{prefijo}_{i+1}" for i in range(max_items)]
+    )
+    return pd.concat([df.drop(columns=[columna_base]), nuevas_cols], axis=1)
 
 agrupado = expandir_columnas(agrupado, 'NUMERO_DOCUMENTO', 'NUMERO_DOCUMENTO')
 agrupado = expandir_columnas(agrupado, 'NOMBRE', 'NOMBRE')
 agrupado = expandir_columnas(agrupado, 'CORREO_1', 'CORREO')
 agrupado = expandir_columnas(agrupado, 'CELULAR_1', 'CELULAR')
-agrupado.dtypes
+
 agrupado['prima'] = agrupado['prima'].fillna(0).astype('int64')
 for col in agrupado.columns:
     if agrupado[col].dtype == 'float64':
         agrupado[col] = agrupado[col].fillna(0).astype('int64')
-        agrupado.to_csv("emisionesAgrupadas.csv", index=False)
 
+agrupado.to_csv("emisionesAgrupadas.csv", index=False)
